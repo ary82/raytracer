7012beca2b72cac37c7370770cda89877f021a24
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2024-08-13 17:07:16 +0530

    feat: antialiasing

--- color.hh
+++ color.hh
@@ -1,6 +1,7 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "interval.hh"
 #include "vec3.hh"
 
 using color = vec3;
@@ -10,10 +11,10 @@ inline void write_color(std::ostream &out, const color &pixel_color) {
   double g = pixel_color.y();
   double b = pixel_color.z();
 
-  // Translate the [0,1] component values to the byte range [0,255].
-  int rbyte = int(255.999 * r);
-  int gbyte = int(255.999 * g);
-  int bbyte = int(255.999 * b);
+  static const interval intensity(0.000, 0.999);
+  int rbyte = int(256 * intensity.clamp(r));
+  int gbyte = int(256 * intensity.clamp(g));
+  int bbyte = int(256 * intensity.clamp(b));
 
   // Write out the pixel color components.
   out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
