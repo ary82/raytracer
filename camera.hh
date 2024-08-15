e334db2e2bc553f7a004b926b38723e6319a2951
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2024-08-15 19:45:55 +0530

    feat: implement diffuse body

--- camera.hh
+++ camera.hh
@@ -4,6 +4,8 @@
 #include "color.hh"
 #include "common.hh"
 #include "hittable.hh"
+#include "ray.hh"
+#include "vec3.hh"
 #include <iostream>
 
 class camera {
@@ -11,6 +13,7 @@ public:
   double aspect_ratio = 1.0;  // Ratio of image width over height
   int image_width = 100;      // Rendered image width in pixel count
   int samples_per_pixel = 10; // Count of random samples for each pixel
+  int max_depth = 10;         // Maximum number of ray bounces into scene
 
   void render(const hittable &world) {
     initialize();
@@ -24,7 +27,7 @@ public:
         color pixel_color(0, 0, 0);
         for (int sample = 0; sample < samples_per_pixel; sample++) {
           ray r = get_ray(i, j);
-          pixel_color += ray_color(r, world);
+          pixel_color += ray_color(r, max_depth, world);
         }
         write_color(std::cout, pixel_samples_scale * pixel_color);
       }
@@ -70,11 +73,17 @@ private:
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
   }
 
-  color ray_color(const ray &r, const hittable &world) const {
+  color ray_color(const ray &r, int depth, const hittable &world) const {
     hit_record rec;
 
-    if (world.hit(r, interval(0, infinity), rec)) {
-      return 0.5 * (rec.normal + color(1, 1, 1));
+    if (depth <= 0) {
+      return color(0, 0, 0);
+    }
+
+    if (world.hit(r, interval(0.001, infinity), rec)) {
+      vec3 direction = rec.normal + random_unit_vector();
+      return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+      // return 0.5 * (rec.normal + color(1, 1, 1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
